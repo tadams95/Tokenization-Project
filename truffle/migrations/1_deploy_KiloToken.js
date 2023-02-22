9f0712b29b83df95f4dc562823603966efa7d0a6
tadams95 <tyrelleadams28@gmail.com>
2023-02-21 19:44:46 -0800

    Setup dotenv file for config

--- truffle/migrations/1_deploy_KiloToken.js
+++ truffle/migrations/1_deploy_KiloToken.js
@@ -2,10 +2,13 @@ const KiloToken = artifacts.require("KiloToken");
 
 const KiloTokenSale = artifacts.require("KiloTokenSale");
 
+require("dotenv").config({ path: "../../.env" });
+// console.log(process.env);
+
 module.exports = async function (deployer) {
   let addr = await web3.eth.getAccounts();
-  await deployer.deploy(KiloToken, 10000000000);
+  await deployer.deploy(KiloToken, process.env.INITIAL_TOKENS);
   await deployer.deploy(KiloTokenSale, 1, addr[0], KiloToken.address);
   let instance = await KiloToken.deployed();
-  await instance.transfer(KiloTokenSale.address, 10000000000);
+  await instance.transfer(KiloTokenSale.address, process.env.INITIAL_TOKENS);
 };
